f82b74a04080147d4928e41246bf2d928a9b19e2
Shotem <vicente.garzara@uanl.edu.mx>
2021-01-30 11:35:37 -0600

    First commit

---
+++ autosetup.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install -y man info vim curl wget sed xclip lynx git file cmake build-essential
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
