f0329dd6f6b88935fb70977600fe9a31c0c48c64
Vicente Garza Reyna <vicente@localhost.localdomain>
2021-03-12 20:02:36 -0600

    Reinstall ssh

--- autosetup.sh
+++ autosetup.sh
@@ -2,4 +2,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y man info vim curl wget sed xclip lynx git file cmake build-essential
 sudo apt install -y zsh
+sudo apt remove ssh
+sudo apt install ssh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
